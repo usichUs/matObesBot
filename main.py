8086b788518da365440194d296748c4090139ba1
UsichUs <guyfoks737@gmail.com>
2022-12-23 02:07:39 +0300

    add interesting function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #Modules
-from settings import TOKEN, ADMIN_ID
+from settings import TOKEN, SET_STICKERS_ID
 import logging
 import time
+import random
 
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -10,20 +11,38 @@ from aiogram import Bot, Dispatcher, executor, types
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
 
+HELP_TXT = '''
+/description - описание бота
+/help - команды бота
+'''
+
+DESCRIPTION_TXT = '''
+Бот, который отправляет смешнявки
+'''
+
+async def on_startup(_):
+    print('running')
 
 #Handlers
+@dp.message_handler(commands=['description'])
+async def description(message: types.Message):
+    await message.reply(text=DESCRIPTION_TXT)
+
+@dp.message_handler(commands=['help'])
+async def description(message: types.Message):
+    await message.reply(text=HELP_TXT)
+
 @dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message()):
-    user_id = message.from_user.id
-    user_full_name = message.from_user.full_name
-    user_name = message.from_user.first_name
-    logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
+async def start_command(message: types.Message):
+    await message.answer(text='<em>Никита сделал бота - GC!!!</em>', parse_mode='HTML')
 
-    await message.reply(f'Спасибо {user_name} за то, что добавил меня!')
-
-
+@dp.message_handler()
+async def random_sticker_reply(message: types.Message or types.Sticker):
+    k = random.randint(0, 6)
+    if k == 3:
+        await bot.send_sticker(message.from_user.id, sticker=random.choice(SET_STICKERS_ID))
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, on_startup=on_startup)
 
     
